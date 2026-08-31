a8be03db7a4e5b028ed3e7432fabadfcc6115f4d
FrankChen <frankchencjc@outlook.com>
2026-08-31 22:08:49 +0800

    fix not work on not win system

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,23 +1,31 @@
 return {
   "mrcjkb/rustaceanvim",
-  cond = vim.fn.has("win32") == 1,
-  opts = {
-    server = {
-      cmd_env = {
-        RUSTUP_TOOLCHAIN = "stable-x86_64-pc-windows-gnullvm",
-      },
-      default_settings = {
-        ["rust-analyzer"] = {
-          cargo = {
-            allFeatures = true,
-            target = "x86_64-pc-windows-gnullvm",
+  opts = function()
+    if vim.fn.has("win32") == 1 then
+      return {
+        server = {
+          cmd_env = {
+            RUSTUP_TOOLCHAIN = "stable-x86_64-pc-windows-gnullvm",
           },
-          check = {
-            command = "clippy",
-            target = "x86_64-pc-windows-gnullvm",
+          default_settings = {
+            ["rust-analyzer"] = {
+              cargo = {
+                allFeatures = true,
+                target = "x86_64-pc-windows-gnullvm",
+              },
+              check = {
+                command = "clippy",
+                target = "x86_64-pc-windows-gnullvm",
+              },
+            },
           },
         },
-      },
-    },
-  },
+      }
+    else
+      return {
+        cargo = { allFeatures = true },
+        check = { command = "clippy" },
+      }
+    end
+  end,
 }

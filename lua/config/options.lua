6eae877ed68ab9a75925edafecfae0eecda19c20
FrankChen <frankchencjc@outlook.com>
2026-09-04 21:08:49 +0800

    fix more

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,7 @@
 if vim.g.neovide then
   if vim.fn.has("win32") == 1 then
     vim.o.guifont = "Iosevka_Nerd_Font,Noto_Sans_CJK_SC,Noto_Color_Emoji:h12"
+    vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.expand("$LOCALAPPDATA") .. "\\Microsoft\\WinGet\\Links"
   elseif vim.fn.has("macunix") == 1 then
     vim.o.guifont = "Iosevka_Nerd_Font,Noto_Sans_Mono_SC,Noto_Color_Emoji:h16"
   end
@@ -13,7 +14,6 @@ if vim.g.neovide then
   vim.g.neovide_scale_factor = 1.0
   vim.g.neovide_cursor_short_animation_length = 1.0
   vim.g.neovide_cursor_trail_size = 0.0
-  if vim.fn.has("win32") == 1 then
-    vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.expand("$LOCALAPPDATA") .. "\\Microsoft\\WinGet\\Links"
-  end
+  vim.g.neovide_opacity = 0.85
+  vim.g.neovide_normal_opacity = 0.85
 end

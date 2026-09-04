042654f58c1731f6d77dc84f57a7bc43c88cbf1b
FrankChen <frankchencjc@outlook.com>
2026-09-04 19:43:20 +0800

    fix mac font size

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 if vim.g.neovide then
-  vim.o.guifont = "Iosevka_Nerd_Font_Mono,Fira_Code_Nerd_Font_Mono,Noto_Sans_Mono_CJK_SC,Noto_Color_Emoji:h12"
+  if vim.fn.has("win32") then
+    vim.o.guifont = "Iosevka_Nerd_Font_Mono,Noto_Sans_Mono_CJK_SC,Noto_Color_Emoji:h12"
+  elseif vim.fn.has("macunix") then
+    vim.o.guifont = "Iosevka_Nerd_Font_Mono,Noto_Sans_Mono_CJK_SC,Noto_Color_Emoji:h16"
+  end
   vim.g.neovide_window_blurred = true
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
@@ -10,6 +14,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_short_animation_length = 1.0
   vim.g.neovide_cursor_trail_size = 0.0
   if vim.fn.has("win32") == 1 then
-      vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.expand("$LOCALAPPDATA") .. "\\Microsoft\\WinGet\\Links"
+    vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.expand("$LOCALAPPDATA") .. "\\Microsoft\\WinGet\\Links"
   end
 end

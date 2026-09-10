1326407c6c3161a4886f3594a552d3d914a8e32a
FrankChen <frankchencjc@outlook.com>
2026-09-10 23:58:15 +0800

    fix meta not as option on mac

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ if vim.g.neovide then
     vim.o.guifont = "Iosevka_Nerd_Font,Noto_Sans_CJK_SC,Noto_Color_Emoji:h12"
     vim.env.PATH = vim.env.PATH .. ";" .. vim.fn.expand("$LOCALAPPDATA") .. "\\Microsoft\\WinGet\\Links"
   elseif vim.fn.has("macunix") == 1 then
+    vim.g.neovide_input_macos_option_key_is_meta = "only_left"
     vim.o.guifont = "Iosevka_Nerd_Font,Noto_Sans_Mono_SC,Noto_Color_Emoji:h16"
   end
   vim.g.neovide_window_blurred = true

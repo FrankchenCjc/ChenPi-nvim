7fda26f17c8fa13c711df005bd120bd5776541a6
FrankChen <frankchencjc@outlook.com>
2026-09-20 15:37:19 +0800

    20260920 yazi usein
    CHANGE:
    将文件浏览改为了yazi，还需要适应，但我感觉我并不需要neotree那样的方式来管理文件

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,12 +1,13 @@
 return {
   {
-    "folke/snacks.nvim",        -- 目标：已存在的插件（不是新建一个）
-    opts = {                    -- "把下面这些合并进它现有的配置"
+    "folke/snacks.nvim", -- 目标：已存在的插件（不是新建一个）
+    opts = { -- "把下面这些合并进它现有的配置"
+      explorer = { enabled = false },
       image = {
-        enabled = true,         -- snacks 内部的 image 模块：开
+        enabled = true, -- snacks 内部的 image 模块：开
         doc = {
-          inline = true,        -- 文档（markdown/html）里的图片：直接内联渲染
-          float = true,         -- 打开图片文件时：弹浮动窗显示
+          inline = true, -- 文档（markdown/html）里的图片：直接内联渲染
+          float = true, -- 打开图片文件时：弹浮动窗显示
         },
       },
     },
---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    dependencies = { { "nvim-lua/plenary.nvim", lazy = true } },
+    keys = {
+      { "<leader>e", "<cmd>Yazi<cr>", desc = "yazi：光标所在文件" },
+      { "<leader>E", "<cmd>Yazi cwd<cr>", desc = "yazi：当前工作目录" },
+      { "<c-up>", "<cmd>Yazi toggle<cr>", desc = "恢复上次 yazi 会话" },
+    },
+    opts = {
+      open_for_directories = false,
+    },
+  },
+}
